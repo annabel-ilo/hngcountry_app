e9daadef50cc3d6c5f7113591f66856758af39f6
Annabel <annabelilo2017@gmail.com>
2022-11-12 14:28:53 +0100

    first Page UI completed

--- lib/screen/all_contries.dart
+++ lib/screen/all_contries.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'country_details.dart';
 
 class AllCountries extends StatefulWidget {
-  const AllCountries({ Key? key }) : super(key: key);
+  const AllCountries({Key? key}) : super(key: key);
 
   @override
   State<AllCountries> createState() => _AllCountriesState();
@@ -10,8 +12,176 @@ class AllCountries extends StatefulWidget {
 class _AllCountriesState extends State<AllCountries> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 14, 7, 45),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 14, 7, 45),
+        title: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: RichText(
+            text: const TextSpan(
+                text: 'Explore',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '.',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.orange,
+                    ),
+                  ),
+                ]),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 22, right: 22),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 10),
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 67, 63, 83),
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                ),
+                child: TextField(
+                  onChanged: (value) {
+                    // print(value);
+                    // //model.searchExploreCauses(value);
+                    // if (value.isEmpty) {
+                    //   setState(() {
+                    //     _isSearching = false;
+                    //   });
+                    // } else {
+                    //   setState(() {
+                    //     _isSearching = true;
+                    //   });
+                    //   Provider.of<CauseProvider>(context, listen: false)
+                    //       .searchExploreCauses(value);
+                    // }
+                  },
+                  //controller: searchController,
+                  textDirection: TextDirection.ltr,
+                  decoration: const InputDecoration(
+                    hintText: "Search Country",
+                    hintStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    contentPadding: EdgeInsets.all(8),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Filter(
+                    icon: Icons.wallet_membership_rounded,
+                    text: 'EN',
+                  ),
+                  Filter(
+                    icon: Icons.wallet_membership_rounded,
+                    text: 'Filter',
+                  ),
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 1.5,
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return TextButton(
+                        onPressed: () => Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => const CountryDetails()),
+                        ),
+                        child: const ListTile(
+                          leading:
+                              Icon(Icons.ac_unit_outlined, color: Colors.white),
+                          title: Text(
+                            'Country Name',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                          subtitle: Text(
+                            'Country CApital',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Filter extends StatelessWidget {
+  const Filter({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child:Text('Countries')
+      height: 50,
+      //width: 50,
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(6)),
+        border: Border.all(
+          color: Colors.white,
+          //width: 1,
+        ),
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Icon(
+              icon,
+              color: Colors.white,
+            ),
+          ),
+          //SizedBox(width: 5),
+          Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screen/country_details.dart
+++ lib/screen/country_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 class CountryDetails extends StatefulWidget {
-  const CountryDetails({ Key? key }) : super(key: key);
+  const CountryDetails({Key? key}) : super(key: key);
 
   @override
   State<CountryDetails> createState() => _CountryDetailsState();
@@ -9,8 +10,48 @@ class CountryDetails extends StatefulWidget {
 class _CountryDetailsState extends State<CountryDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Text('Countries details')
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 14, 7, 45),
+      // appBar: AppBar(
+      //   backgroundColor: const Color.fromARGB(255, 14, 7, 45),
+      //   title: Padding(
+      //     padding: const EdgeInsets.all(10.0),
+      //     child: RichText(
+      //       text: const TextSpan(
+      //           text: 'Explore',
+      //           style: TextStyle(
+      //               fontSize: 30,
+      //               fontWeight: FontWeight.w500,
+      //               color: Colors.white),
+      //           children: <TextSpan>[
+      //             TextSpan(
+      //               text: '.',
+      //               style: TextStyle(
+      //                 fontSize: 40,
+      //                 fontWeight: FontWeight.w800,
+      //                 color: Colors.orange,
+      //               ),
+      //             ),
+      //           ]),
+      //     ),
+      //   ),
+      // ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 22, right: 22),
+          child: Column(
+            children: [
+              Container(
+                  margin: const EdgeInsets.only(top: 10, bottom: 10),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 67, 63, 83),
+                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                  ),
+                  child: Text('yiyiyiiy')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
